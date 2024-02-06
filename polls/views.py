7742fdfe1f84bbfd9d0ee32d07090039f4627fcf
Stan1-0 <stankofb@gmail.com>
2024-02-06 20:25:19 +0000

    Refactored views to use generic views

--- polls/views.py
+++ polls/views.py
@@ -2,29 +2,31 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from polls.models import Question, Choice
+from django.views import generic 
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.Listview):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+    
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class detail(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
 
-def detail(request, question_id):
-    question =get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return  render(request, 'polls/results.html', {'question': question})
-
+class Resultsview(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
 def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        retun render(
+        return render(
             request,
             'polls/detail.html',
             {
